84ee01a601eb32b5f9fbf50c902e3556e388536f
meow3azy <120301699+meow3azy@users.noreply.github.com>
2024-03-19 21:59:51 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -6,6 +6,7 @@ input_str = "hello, world!"
 result = uppercase_string(input_str)
 print(result)
 def uppercase_string(input_string):
-
     return input_string.upper()
-
+input_str = "hello, world!"
+result = convert_to_title_case(input_str)
+print(result)
