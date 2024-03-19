919e3288b4ecd12e34a1b5f45ef9d812933def34
meow3azy <120301699+meow3azy@users.noreply.github.com>
2024-03-19 21:52:31 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -5,3 +5,15 @@ def uppercase_string(input_string):
 input_str = "hello, world!"
 result = uppercase_string(input_str)
 print(result)
+def uppercase_string(input_string):
+    """
+    Принимает строку и возвращает ее с заглавными буквами.
+
+    Args:
+    input_string (str): Входная строка.
+
+    Returns:
+    str: Строка с заглавными буквами.
+    """
+    return input_string.upper()
+
