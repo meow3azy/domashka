ca6a892bc900789d09f126c83930360aeefc88e6
Pavel Isaev <killroy1297@gmail.com>
2024-03-19 22:03:35 +0300

    new

--- str_func.py
+++ str_func.py
@@ -5,3 +5,9 @@ def uppercase_string(input_string):
 input_str = "hello, world!"
 result = uppercase_string(input_str)
 print(result)
+def convert_to_title_case(input_string):
+    return input_string.title()
+# Пример использования функции
+input_str = "hello, world!"
+result = uppercase_string(input_str)
+print(result)
